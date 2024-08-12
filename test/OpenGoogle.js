11c118eb681410c4c5572817c121940ae5aaa396
mrverma441 <monuv6342@gmail.com>
2024-08-12 19:28:47 +0530

    Initial commit

---
+++ test/OpenGoogle.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'Open Google Test': function (browser) {
+      browser.maximizeWindow()
+        .url('https://www.google.com')       
+        .waitForElementVisible('body', 1000) 
+        .assert.titleContains('Google').setValue('[title="Search"]', ['https://linkedin.com/in/monu-verma-8964161a3', browser.Keys.ENTER])
+        .waitForElementVisible(by.xpath('//*[text()="Monu Verma - Automation Consultant Level 2 - NashTech"]'), 10000).click(by.xpath('//*[text()="Monu Verma - Automation Consultant Level 2 - NashTech"]'))
+        .click('#base-contextual-sign-in-modal > div > section > button > icon > svg').assert.elementPresent(by.xpath('//*[normalize-space(text())="Automation Consultant at NashTech ||Ex-infoscian|| ISTQB Certified Foundation Level || ISTQB Certified Agile Tester"]'))
+        .pause()                           
+    }
+  };
+  
