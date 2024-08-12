5b9c80253d2bb1112b691e486650c2a8dd6e1eb8
mrverma441 <monuv6342@gmail.com>
2024-08-12 21:15:26 +0530

    Added lint

--- test/OpenGoogle.js
+++ test/OpenGoogle.js
@@ -6,7 +6,8 @@ module.exports = {
         .assert.titleContains('Google').setValue('[title="Search"]', ['https://linkedin.com/in/monu-verma-8964161a3', browser.Keys.ENTER])
         .waitForElementVisible(by.xpath('//*[text()="Monu Verma - Automation Consultant Level 2 - NashTech"]'), 10000).click(by.xpath('//*[text()="Monu Verma - Automation Consultant Level 2 - NashTech"]'))
         .click('#base-contextual-sign-in-modal > div > section > button > icon > svg').assert.elementPresent(by.xpath('//*[normalize-space(text())="Automation Consultant at NashTech ||Ex-infoscian|| ISTQB Certified Foundation Level || ISTQB Certified Agile Tester"]'))
-        .pause()                           
+        .end();
+        // hello how are you                      
     }
   };
   
